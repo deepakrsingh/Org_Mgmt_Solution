ba115466692090826da2af642b87bac31856fd9c
Keshav Sharma <sharma.keshav39@gmail.com>
2025-07-04 21:28:19 +0530

    Create employee table using EF Code First migrations

---
+++ OMSWeb/Data/OrgDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OMSWeb.Modelsl;
+
+namespace OMSWeb.Data;
+
+public class OrgDbContext : DbContext
+{
+    public OrgDbContext(DbContextOptions<OrgDbContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>()
+            .ToTable("Employees");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ OMSWeb/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace OMSWeb.Modelsl;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? Occuptation { get; set; }
+    public required DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
